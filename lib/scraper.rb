116b80125ab694a55b8f397d66d70d3693ad0537
Christopher Cid <iamcid17@gmail.com>
2020-04-07 02:29:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,5 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc = Nokogiri::HTML(html)
 puts doc.css(".headline-260IBN").text
